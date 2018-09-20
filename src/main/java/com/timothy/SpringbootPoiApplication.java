358d42e885c7c618625201227702e14b46272c8b
tanzixuan <tzx0120@163.com>
2018-09-20 09:56:53 +0800

    测试连接

--- src/main/java/com/timothy/SpringbootPoiApplication.java
+++ src/main/java/com/timothy/SpringbootPoiApplication.java
@@ -9,7 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootPoiApplication {
 
 	public static void main(String[] args) {
-		//测试
 		SpringApplication.run(SpringbootPoiApplication.class, args);
 	}
 }
